9459c5fcc4a050df160a80a45282ad4f93401497
niihaad25 <nihadbensalah2@gmail.com>
2024-01-01 20:17:18 +0100

    v2

--- prueba.cpp
+++
@@ -1,8 +0,0 @@
-#include <<iostream>>
-using namespace std;
-
-int main() {
-    const int myConstant = 10;  // Exemple de constant amb valor 10
-    cout << "La constant és: " << myConstant << endl;
-    return 0;
-}
